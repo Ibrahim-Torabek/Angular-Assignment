410e74ffc6c75cdc399d77a8175dc7cdcfd9d557
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-09-21 00:20:30 -0400

    Content-card component created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent
   ],
   imports: [
     BrowserModule
