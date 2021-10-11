8d165f7539842f17add16219269a159ce6bc17b1
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-10-11 13:15:14 -0400

    added mouseenter and mouseleave host listener.

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective {
 
-  constructor() { }
+  private elm: ElementRef;
+
+
+  constructor(e: ElementRef) {
+    this.elm = e;
+  }
+
+  ngOnInit(): void{
+
+  }
+
+  @HostListener('mouseenter') onMouseEnter(){
+
+    this.elm.nativeElement.style.textDecoration = "underline";
+    //console.log("Mouse Entered")
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.elm.nativeElement.style.textDecoration = 'none';
+  }
 
 }
