86310fd63b22a25f82b71448785588ab47d95711
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-10-11 12:36:22 -0400

    Hover-Style directive generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentTypePipe } from './content-type.pipe';
+import { HoverDirective } from './hover.directive';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { ContentTypePipe } from './content-type.pipe';
     ContentCardComponent,
     ContentListComponent,
     ContentTypePipe,
+    HoverDirective,
 
   ],
   imports: [
