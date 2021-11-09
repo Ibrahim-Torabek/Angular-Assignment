586193e32fd5cede88b92b2407e4f6518d7bda3b
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-11-08 21:32:44 -0500

    Assignment 6 completed

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import {Observable, of} from "rxjs";
 import {Content} from "../helper-files/content-interface";
 import {CONTENT} from "../helper-files/contentDb";
+import {MessageService} from "./message.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // In this case we didn't get contents synchronously,
   // So, I named the function getContents, not getContentObs.
   getContents():Observable<Content[]>{
+    this.messageService.add("Contents retrieved successfully!")
+
     return of(CONTENT);
   }
 }
